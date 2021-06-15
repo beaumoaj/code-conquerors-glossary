7baeb97fbdfee62be65c152889b92b2616ccd144
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-15 16:13:45 +0100

    css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Searchbar from './Searchbar';
 import './App.css';
+import Data from "./listOfTerms";
 
 function App() {
   return (
     <div className="App">
-      Glossary
+      <h1>Glossary</h1>
       <Searchbar/>
+      {/* {listOfTerms.map} */}
     </div>
 
   );
