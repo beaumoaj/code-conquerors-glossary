e770e3ed27c1b7ec2411c5d3d40e12fd2f8ddf36
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-15 15:22:54 +0100

    deleted unused files
    cleaned up react up and named it glossary

---
+++ src/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Searchbar() {
+    return (
+        <div type="text" placeholder="Search...">
+            
+        </div>
+    )
+}
+
+export default Searchbar
