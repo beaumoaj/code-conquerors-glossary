31dc9eaafe77214793a7ff9faeaebf92955c7731
victoria ukaonu <73253494+Sozchi@users.noreply.github.com>
2021-06-28 23:58:44 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import AddNewTerm from './AddNewTerm';
 // import UpdatingTheTerms from './UpdatingTheTerms';
 import AddNewTermBtn from './AddNewTermBtn';
 import DeleteTerm from './DeleteTerm';
-import ListOfTerms from './ListOfTerms';
+// import ListOfTerms from './ListOfTerms';
 import ResourcesList from './ResourcesList';
 import ResourceAdd from './ResourceAdd';
 import HomePage from './HomePage';
-import DataOfTerms from "./DataOfTerms";
+// import DataOfTerms from "./DataOfTerms";
 import ResultPage from "./ResultPage"
-import Popup from './Popup';
+import { BrowserRouter, Links, Router } from "react-router-dom";
+// import Popup from './Popup';
 
 
 
@@ -26,13 +27,13 @@ function App() {
   const [token, setToken] = useState("");
   const [showBtn, setShowBtn] = useState(true);
   const [open, setOpen] = useState(true);
-  const [res, setRes] = useState([]);
+  // const [res, setRes] = useState([]);
 
   const handleLoginClick = () => {
     setShowBtn((showBtn) => !showBtn)
   };
 
-  const [password, setPassword] = useState("");
+  // const [password, setPassword] = useState("");
   useEffect(() => {
     fetch("https://wm2-glossary.herokuapp.com/api/terms/")
       .then((res) => res.json())
@@ -50,22 +51,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>{`The length of the list is ${terms.length}`}</div>
+        {/* <div>{`The length of the list is ${terms.length}`}</div> */}
         <h1>Terms Definition</h1>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+        <BrowserRouter>
+          <HomePage />
+          <Login setToken={setToken} open={open} setOpen={setOpen} />
+          {/* <ListOfTerms /> */}
 
-        <Login setToken={setToken} open={open} setOpen={setOpen} />
-        <ListOfTerms />
+          <SearchButton search={search} setSearch={setSearch} />
 
-        <SearchButton search={search} setSearch={setSearch} />
-        <HomePage />
-        <DataOfTerms />
-        <ResultPage />
-        <Terms terms={terms} search={search} /> */}
-        <ResourcesList token={token} />
-        <AddNewTermBtn handleLoginClick={handleLoginClick} />
-        <AddNewTerm token={token} showBtn={showBtn} />
-        <ResourceAdd token={token} />
-        <DeleteTerm token={token} />
+          {/* <DataOfTerms /> */}
+          <ResultPage />
+          <Terms terms={terms} search={search} />
+          <ResourcesList token={token} />
+          <AddNewTermBtn handleLoginClick={handleLoginClick} />
+          <AddNewTerm token={token} showBtn={showBtn} />
+          <ResourceAdd token={token} />
+          <DeleteTerm token={token} />
+        </BrowserRouter>
+
         {/* {/* <UpdatingTheTerms token={token} setToken={setToken} /> */}
       </header>
     </div>
